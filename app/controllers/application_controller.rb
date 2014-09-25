a2893c3dd56f775ec06cb4e39caa541b1e9bd4f8
markwiggles <markwigg@live.com>
2014-09-25 19:24:26 +1000

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  layout 'default'
+
+  before_filter 'assign_links'
+
+
+  def assign_links
+
+    @twitter = 'https://twitter.com/Laurasiaband'
+    @facebook = 'https://www.facebook.com/laurasiaband'
+    @youtube = ''
+    @tumblr = ''
+    @instagram = ''
+
+    @newmessage = 'first release'
+
+  end
+
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+  def index
+
+    @artist_name = 'Laurasia'
+
+  end
+
+
+end
